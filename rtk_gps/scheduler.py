82e5e19978e25353f47febecedb894fa39905c52
thordurka <thordurka@vedur.is>
2024-03-19 10:35:51 +0000

    fixed some bugs and added exception handling

--- rtk_gps/scheduler.py
+++ rtk_gps/scheduler.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 import libnfs
-from rtk_gps.rtk_gps import plot_rtk_neu
+from rtk_gps import plot_rtk_neu
 import schedule
 import paramiko
 
@@ -48,7 +48,7 @@ def plot(nfs: libnfs.NFS, figure_path: str):
         )
         plot_rtk_neu(
             nfs,
-            baseline_list,
+            baselines,
             start=start,
             end=end,
             resample=resample_str,
@@ -58,7 +58,7 @@ def plot(nfs: libnfs.NFS, figure_path: str):
         )
         plot_rtk_neu(
             nfs,
-            baseline_list,
+            baselines,
             start=start,
             end=end,
             resample=resample_str,
@@ -75,10 +75,6 @@ def program_schedule():
     if os.path.exists(figure_path) == False:
         os.mkdir(figure_path)
 
-    logging.info("Mounting NFS...")
-    nfs = libnfs.NFS(os.path.join(os.environ.get("RTK_NFS_HOST"), os.environ.get("RTK_NFS_PATH")))
-    plot(nfs, figure_path)
-
     private_key_str = os.environ.get("RTK_SSH_PRIVATE_KEY")
 
     if not private_key_str:
@@ -86,27 +82,40 @@ def program_schedule():
 
     private_key = paramiko.RSAKey.from_private_key(io.StringIO(private_key_str))
 
+    logging.info("Mounting NFS...")
+    NFS_HOST = os.environ.get("RTK_NFS_HOST")
+    NFS_PATH = os.environ.get("RTK_NFS_PATH")
+    try
+        nfs = libnfs.NFS(os.path.join(NFS_HOST, NFS_PATH))
+        plot(nfs, figure_path)
+    except:
+        logging.error(f"Failed to mount NFS at {NFS_HOST}")
+        return
 
     logging.info("Establishing SSH connection to CDN...")
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(
-        os.environ.get("RTK_SFTP_HOST"),
-        username=os.environ.get("RTK_SFTP_USER"),
-        pkey=private_key
-    )
+    SFTP_HOST = os.environ.get("RTK_SFTP_HOST")
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(
+            SFTP_HOST,
+            username=os.environ.get("RTK_SFTP_USER"),
+            pkey=private_key
+        )
 
-    sftp = client.open_sftp()
-    remote_folder_path = os.environ.get("RTK_SFTP_PATH")
-    
-    for fn in os.listdir(figure_path):
-        local_path = os.path.join(figure_path, fn)
-        remote_path = os.path.join(remote_folder_path, fn)
-        sftp.put(local_path, remote_path)
-        logging.info(f"{fn} transferred.")
+        sftp = client.open_sftp()
+        remote_folder_path = os.environ.get("RTK_SFTP_PATH")
+        
+        for fn in os.listdir(figure_path):
+            local_path = os.path.join(figure_path, fn)
+            remote_path = os.path.join(remote_folder_path, fn)
+            sftp.put(local_path, remote_path)
+            logging.info(f"{fn} transferred.")
 
-
-    client.close()
+        client.close()
+    except:
+        logging.error(f"Failed to establish an SSH connection to {SFTP_HOST}")
+        return
     
 def main():
     sys.excepthook = handle_uncaught_exception
